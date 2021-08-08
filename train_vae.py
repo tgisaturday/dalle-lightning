3c27f2d87b7f1ebecaddd94d8a43a1a812d365d8
taehoon.kim <jamesk1228@gmail.com>
2021-08-08 23:50:31 +0900

    found out how to connect tensorboard with tpu

--- train_vae.py
+++ train_vae.py
@@ -228,8 +228,8 @@ if __name__ == "__main__":
                 print("Setting default ckpt to {}. If this is unexpected behavior, remove {}".format(ckpt_path, ckpt_path))
 
     if args.wandb:
-        logger = pl.loggers.wandb.WandbLogger(project='vqvae', log_model='all', group='TPU')
-        #logger.watch(model)
+        logger = pl.loggers.wandb.WandbLogger(project='vqvae', log_model='all')
+        logger.watch(model)
     else:
         logger = pl.loggers.tensorboard.TensorBoardLogger(args.log_dir)                
     if args.use_tpus:
