d96c2d2a058e06c0e3a1931cc4a46252e0e40f84
taehoon.kim <jamesk1228@gmail.com>
2021-08-11 19:30:59 +0900

    fix size mismatch error for log image dalle

--- pl_dalle/models/dalle.py
+++ pl_dalle/models/dalle.py
@@ -579,6 +579,7 @@ class DALLE(pl.LightningModule):
         if self.args.log_images:
             img_logits = logits[:, self.text_seq_len:].long()
             img_seq = torch.argmax(img_logits, dim = 1).flatten(1)
+            print(img_seq).shape()
             xrec = self.vae.decode(img_seq, feed_seq=True) 
             return loss, loss_text, loss_img, xrec 
         else:     
