e2d716288c32ad59d01b391246dadfe375f51561
taehoon.kim <jamesk1228@gmail.com>
2021-08-13 11:15:36 +0900

    wds creator update

--- pl_dalle/utils/wds_create_whole.py
+++ pl_dalle/utils/wds_create_whole.py
@@ -4,55 +4,102 @@ import os
 from pathlib import Path
 from collections import Counter
 from PIL import Image
-
-DATASETPATH = 'data'
-OUTPUTFILENAME = 'dataset'
-
-alldirs = os.walk(Path(DATASETPATH))
-all_basepaths = []
-
-############################################################################
-########### Legacy wds_creator - better use wds_create_shards.py ###########
-############################################################################
-
-### (1) Find image-text pairs in all (sub)folders of the basepath
-
-for dir in alldirs:
-    fns = dir[2]
-    if next((True for x in fns if '.txt' in x), False):
-        basenames = [x.split('.')[0] for x in fns]
-        basepaths = [dir[0] + '/' + k for k, v in dict(Counter(basenames)).items() if v == 2]
-        all_basepaths.extend(basepaths)
-
-all_basepaths = sorted(all_basepaths, key=lambda x: x.split('/')[-1])
-curated_basepaths = []
+import argparse, sys, random, glob
 
 
-### (2) Verify images exist and can be opened
+parser = argparse.ArgumentParser("""Generate sharded dataset from image-text-datasets.""")
+parser.add_argument(
+    "--image_text_keys", 
+    type=str, 
+    default="img,cap",
+    help="Comma separated WebDataset dictionary keys for images (first argument) and texts (second argument). \
+          The exact argument has to be provided to train_dalle.py, e.g. python train_dalle.py --wds img,cp --image_text_folder ../shards"
+    )
+parser.add_argument(
+    "--compression", 
+    dest="compression", 
+    action="store_true",
+    help="Creates compressed .tar.gz files instead of uncompressed .tar files."
+    )    
+parser.add_argument(
+    "--output", 
+    default="./dataset", 
+    help="directory where shards are written"
+)
+parser.add_argument(
+    "--shard_prefix", 
+    default="ds_", 
+    help="prefix of shards' filenames created in the shards-folder"
+)
+parser.add_argument(
+    "--data",
+    default="./data",
+    help="directory path containing data suitable for DALLE-pytorch training",
+)
+args = parser.parse_args()
 
-for basepath in all_basepaths:
-    img = Image.open(Path(basepath + '.jpg'))
+image_key, caption_key = tuple(args.image_text_keys.split(','))
+
+if not os.path.isdir(os.path.join(args.data)):
+    print(f"{args.data}: should be directory containing image-text pairs", file=sys.stderr)
+    print(f"or subfolders containing image-text-pairs", file=sys.stderr)
+    sys.exit(1)
+
+os.makedirs(Path(args.output), exist_ok=True)
+
+def readfile(fname):
+    "Read a binary file from disk."
+    with open(fname, "rb") as stream:
+        return stream.read()
+
+text_files = [glob.glob(f'{args.data}/**/*.txt', recursive=True)]
+text_files = {text_file.stem: text_file for text_file in text_files} # str(text_file.parents[0]) + 
+text_total = len(text_files)
+
+image_files = [
+    glob.glob(f'{args.data}/**/*.png', recursive=True), glob.glob(f'{args.data}/**/*.jpg', recursive=True),
+    glob.glob(f'{args.data}/**/*.jpeg', recursive=True), glob.glob(f'{args.data}/**/*.bmp', recursive=True)
+]
+image_files = {image_file.stem: image_file for image_file in image_files} # str(image_file.parents[0]) +
+image_total = len(image_files)
+
+print('Found {:,} textfiles and {:,} images.'.format(text_total, image_total))
+
+keys = (image_files.keys() & text_files.keys())
+
+text_files = {k: v for k, v in text_files.items() if k in keys}
+image_files = {k: v for k, v in image_files.items() if k in keys}
+
+for key in image_files:
+    img = Image.open(image_files[key])
     try:
         img.verify()
     except Exception:
-        print('Invalid image on path {}'.format(basepath))
-    else:
-        curated_basepaths.append(basepath)
+        print('Invalid image on path {}'.format(key))
+        keys.remove(key)
 
+print("Remaining keys after image sanity check: {:,}".format(len(keys)))
+
+total_pairs = len(keys)
+keys = list(keys)
+
+indexes = list(range(total_pairs))
+random.shuffle(indexes)
 
 ### (3) Create compressed Webdataset tar file
+if args.compression:
+    sink = wds.TarWriter(f'{args.output}/ds.tar.gz', encoder=False)    
+else:
+    sink = wds.TarWriter(f'{args.output}/ds.tar', encoder=False)
 
-sink = wds.TarWriter(OUTPUTFILENAME + '.tar.gz', encoder=False)
-
-sink.write
-
-for basepath in curated_basepaths:
-    with open(Path(basepath + '.jpg'), "rb") as imgstream:
+for i in indexes:
+    with open(image_files[keys[i]], "rb") as imgstream:
         image = imgstream.read()
-    with open(Path(basepath + '.txt'), "rb") as txtstream:
+    with open(text_files[keys[i]], "rb") as txtstream:
         text = txtstream.read()
+    ds_key = "%09d" % i
     sample = {
-        "__key__": basepath.split('/')[-1],
+        "__key__": ds_key,
         "img": image,
         "cap": text
     }
